03b57e6e6bdd4f9921dd69bd379e0b6bc20f5747
xiaoxiae <tomas.slama.131@gmail.com>
2018-09-16 18:54:49 +0200

    add factorization.py

---
+++ Programy/03 - Rozklad prvočísla/factorization.py
@@ -0,0 +1,19 @@
+from math import sqrt, floor
+
+print("The number to factorize: ", end="")
+number = int(input())
+
+# Go through all the numbers under sqrt(num) and check, if they are factors
+factors = []
+for i in range(2, floor(sqrt(number))):
+    while number % i == 0:
+        number //= i
+        factors.append(i)
+
+# If the number isn't fully divided, add the remainding factor
+if number != 1:
+    factors.append(number)
+
+print("Factors: "+str(factors))
+
+input()
