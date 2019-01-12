3140014235101817f07ac0c1631f01e04c12e8a0
xiaoxiae <tomas.slama.131@gmail.com>
2019-01-12 18:44:34 +0100

    replace "mastermind.py" with a C# version

---
+++ Programy/09 - Hra logik/Mastermind/Mastermind/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace Logik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.color_label_1 = new System.Windows.Forms.Label();
+            this.guess_label_1 = new System.Windows.Forms.Label();
+            this.color_label_4 = new System.Windows.Forms.Label();
+            this.color_label_2 = new System.Windows.Forms.Label();
+            this.color_label_5 = new System.Windows.Forms.Label();
+            this.color_label_6 = new System.Windows.Forms.Label();
+            this.color_label_3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.heading_label = new System.Windows.Forms.Label();
+            this.guess_label_2 = new System.Windows.Forms.Label();
+            this.guess_label_3 = new System.Windows.Forms.Label();
+            this.guess_label_4 = new System.Windows.Forms.Label();
+            this.guess_label_5 = new System.Windows.Forms.Label();
+            this.move_history = new System.Windows.Forms.DataGridView();
+            this.start_game_button = new System.Windows.Forms.Button();
+            this.black_user_input = new System.Windows.Forms.NumericUpDown();
+            this.white_user_input = new System.Windows.Forms.NumericUpDown();
+            this.manual_evaluation_button = new System.Windows.Forms.Button();
+            this.automatic_evaluate_button = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.move_history)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.black_user_input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.white_user_input)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // color_label_1
+            // 
+            this.color_label_1.BackColor = System.Drawing.Color.Red;
+            this.color_label_1.Location = new System.Drawing.Point(9, 8);
+            this.color_label_1.Name = "color_label_1";
+            this.color_label_1.Size = new System.Drawing.Size(47, 51);
+            this.color_label_1.TabIndex = 0;
+            this.color_label_1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.get_color);
+            // 
+            // guess_label_1
+            // 
+            this.guess_label_1.AllowDrop = true;
+            this.guess_label_1.BackColor = System.Drawing.Color.Silver;
+            this.guess_label_1.Location = new System.Drawing.Point(16, 55);
+            this.guess_label_1.Name = "guess_label_1";
+            this.guess_label_1.Size = new System.Drawing.Size(27, 29);
+            this.guess_label_1.TabIndex = 1;
+            this.guess_label_1.DragDrop += new System.Windows.Forms.DragEventHandler(this.color_drag_drop);
+            this.guess_label_1.DragEnter += new System.Windows.Forms.DragEventHandler(this.color_drag_enter);
+            // 
+            // color_label_4
+            // 
+            this.color_label_4.BackColor = System.Drawing.Color.Blue;
+            this.color_label_4.Location = new System.Drawing.Point(9, 64);
+            this.color_label_4.Name = "color_label_4";
+            this.color_label_4.Size = new System.Drawing.Size(47, 51);
+            this.color_label_4.TabIndex = 2;
+            this.color_label_4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.get_color);
+            // 
+            // color_label_2
+            // 
+            this.color_label_2.BackColor = System.Drawing.Color.Green;
+            this.color_label_2.Location = new System.Drawing.Point(62, 8);
+            this.color_label_2.Name = "color_label_2";
+            this.color_label_2.Size = new System.Drawing.Size(47, 51);
+            this.color_label_2.TabIndex = 3;
+            this.color_label_2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.get_color);
+            // 
+            // color_label_5
+            // 
+            this.color_label_5.BackColor = System.Drawing.Color.Gray;
+            this.color_label_5.Location = new System.Drawing.Point(62, 64);
+            this.color_label_5.Name = "color_label_5";
+            this.color_label_5.Size = new System.Drawing.Size(47, 51);
+            this.color_label_5.TabIndex = 4;
+            this.color_label_5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.get_color);
+            // 
+            // color_label_6
+            // 
+            this.color_label_6.BackColor = System.Drawing.Color.Orange;
+            this.color_label_6.Location = new System.Drawing.Point(115, 64);
+            this.color_label_6.Name = "color_label_6";
+            this.color_label_6.Size = new System.Drawing.Size(47, 51);
+            this.color_label_6.TabIndex = 5;
+            this.color_label_6.MouseDown += new System.Windows.Forms.MouseEventHandler(this.get_color);
+            // 
+            // color_label_3
+            // 
+            this.color_label_3.BackColor = System.Drawing.Color.White;
+            this.color_label_3.Location = new System.Drawing.Point(115, 8);
+            this.color_label_3.Name = "color_label_3";
+            this.color_label_3.Size = new System.Drawing.Size(47, 51);
+            this.color_label_3.TabIndex = 6;
+            this.color_label_3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.get_color);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.color_label_3);
+            this.panel1.Controls.Add(this.color_label_1);
+            this.panel1.Controls.Add(this.color_label_6);
+            this.panel1.Controls.Add(this.color_label_4);
+            this.panel1.Controls.Add(this.color_label_5);
+            this.panel1.Controls.Add(this.color_label_2);
+            this.panel1.Location = new System.Drawing.Point(189, 11);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(173, 126);
+            this.panel1.TabIndex = 7;
+            // 
+            // heading_label
+            // 
+            this.heading_label.AutoSize = true;
+            this.heading_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.heading_label.Location = new System.Drawing.Point(13, 11);
+            this.heading_label.Name = "heading_label";
+            this.heading_label.Size = new System.Drawing.Size(168, 33);
+            this.heading_label.TabIndex = 8;
+            this.heading_label.Text = "Mastermind";
+            // 
+            // guess_label_2
+            // 
+            this.guess_label_2.AllowDrop = true;
+            this.guess_label_2.BackColor = System.Drawing.Color.Silver;
+            this.guess_label_2.Location = new System.Drawing.Point(49, 55);
+            this.guess_label_2.Name = "guess_label_2";
+            this.guess_label_2.Size = new System.Drawing.Size(27, 29);
+            this.guess_label_2.TabIndex = 9;
+            this.guess_label_2.DragDrop += new System.Windows.Forms.DragEventHandler(this.color_drag_drop);
+            this.guess_label_2.DragEnter += new System.Windows.Forms.DragEventHandler(this.color_drag_enter);
+            // 
+            // guess_label_3
+            // 
+            this.guess_label_3.AllowDrop = true;
+            this.guess_label_3.BackColor = System.Drawing.Color.Silver;
+            this.guess_label_3.Location = new System.Drawing.Point(82, 55);
+            this.guess_label_3.Name = "guess_label_3";
+            this.guess_label_3.Size = new System.Drawing.Size(27, 29);
+            this.guess_label_3.TabIndex = 10;
+            this.guess_label_3.DragDrop += new System.Windows.Forms.DragEventHandler(this.color_drag_drop);
+            this.guess_label_3.DragEnter += new System.Windows.Forms.DragEventHandler(this.color_drag_enter);
+            // 
+            // guess_label_4
+            // 
+            this.guess_label_4.AllowDrop = true;
+            this.guess_label_4.BackColor = System.Drawing.Color.Silver;
+            this.guess_label_4.Location = new System.Drawing.Point(115, 55);
+            this.guess_label_4.Name = "guess_label_4";
+            this.guess_label_4.Size = new System.Drawing.Size(27, 29);
+            this.guess_label_4.TabIndex = 11;
+            this.guess_label_4.DragDrop += new System.Windows.Forms.DragEventHandler(this.color_drag_drop);
+            this.guess_label_4.DragEnter += new System.Windows.Forms.DragEventHandler(this.color_drag_enter);
+            // 
+            // guess_label_5
+            // 
+            this.guess_label_5.AllowDrop = true;
+            this.guess_label_5.BackColor = System.Drawing.Color.Silver;
+            this.guess_label_5.Location = new System.Drawing.Point(148, 55);
+            this.guess_label_5.Name = "guess_label_5";
+            this.guess_label_5.Size = new System.Drawing.Size(27, 29);
+            this.guess_label_5.TabIndex = 12;
+            this.guess_label_5.DragDrop += new System.Windows.Forms.DragEventHandler(this.color_drag_drop);
+            this.guess_label_5.DragEnter += new System.Windows.Forms.DragEventHandler(this.color_drag_enter);
+            // 
+            // move_history
+            // 
+            this.move_history.AllowUserToAddRows = false;
+            this.move_history.AllowUserToDeleteRows = false;
+            this.move_history.AllowUserToResizeColumns = false;
+            this.move_history.AllowUserToResizeRows = false;
+            this.move_history.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.move_history.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.move_history.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.move_history.ColumnHeadersHeight = 5;
+            this.move_history.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.move_history.ColumnHeadersVisible = false;
+            this.move_history.EnableHeadersVisualStyles = false;
+            this.move_history.Location = new System.Drawing.Point(17, 96);
+            this.move_history.MultiSelect = false;
+            this.move_history.Name = "move_history";
+            this.move_history.ReadOnly = true;
+            this.move_history.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.move_history.RowHeadersVisible = false;
+            this.move_history.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.move_history.Size = new System.Drawing.Size(156, 95);
+            this.move_history.TabIndex = 13;
+            // 
+            // start_game_button
+            // 
+            this.start_game_button.Location = new System.Drawing.Point(189, 144);
+            this.start_game_button.Name = "start_game_button";
+            this.start_game_button.Size = new System.Drawing.Size(56, 47);
+            this.start_game_button.TabIndex = 14;
+            this.start_game_button.Text = "Start game";
+            this.start_game_button.UseVisualStyleBackColor = true;
+            this.start_game_button.Click += new System.EventHandler(this.start_game_button_Click);
+            // 
+            // black_user_input
+            // 
+            this.black_user_input.Enabled = false;
+            this.black_user_input.Location = new System.Drawing.Point(254, 144);
+            this.black_user_input.Name = "black_user_input";
+            this.black_user_input.Size = new System.Drawing.Size(33, 20);
+            this.black_user_input.TabIndex = 15;
+            this.black_user_input.ValueChanged += new System.EventHandler(this.black_user_input_ValueChanged);
+            // 
+            // white_user_input
+            // 
+            this.white_user_input.Enabled = false;
+            this.white_user_input.Location = new System.Drawing.Point(254, 171);
+            this.white_user_input.Name = "white_user_input";
+            this.white_user_input.Size = new System.Drawing.Size(33, 20);
+            this.white_user_input.TabIndex = 16;
+            // 
+            // manual_evaluation_button
+            // 
+            this.manual_evaluation_button.Enabled = false;
+            this.manual_evaluation_button.Location = new System.Drawing.Point(293, 144);
+            this.manual_evaluation_button.Name = "manual_evaluation_button";
+            this.manual_evaluation_button.Size = new System.Drawing.Size(70, 20);
+            this.manual_evaluation_button.TabIndex = 17;
+            this.manual_evaluation_button.Text = "Manual";
+            this.manual_evaluation_button.UseVisualStyleBackColor = true;
+            this.manual_evaluation_button.Click += new System.EventHandler(this.manual_evaluation_button_Click);
+            // 
+            // automatic_evaluate_button
+            // 
+            this.automatic_evaluate_button.Enabled = false;
+            this.automatic_evaluate_button.Location = new System.Drawing.Point(293, 171);
+            this.automatic_evaluate_button.Name = "automatic_evaluate_button";
+            this.automatic_evaluate_button.Size = new System.Drawing.Size(70, 20);
+            this.automatic_evaluate_button.TabIndex = 18;
+            this.automatic_evaluate_button.Text = "Automatic";
+            this.automatic_evaluate_button.UseVisualStyleBackColor = true;
+            this.automatic_evaluate_button.Click += new System.EventHandler(this.automatic_evaluate_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 204);
+            this.Controls.Add(this.automatic_evaluate_button);
+            this.Controls.Add(this.manual_evaluation_button);
+            this.Controls.Add(this.white_user_input);
+            this.Controls.Add(this.black_user_input);
+            this.Controls.Add(this.start_game_button);
+            this.Controls.Add(this.move_history);
+            this.Controls.Add(this.guess_label_5);
+            this.Controls.Add(this.guess_label_4);
+            this.Controls.Add(this.guess_label_3);
+            this.Controls.Add(this.guess_label_2);
+            this.Controls.Add(this.heading_label);
+            this.Controls.Add(this.guess_label_1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Matermind";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.color_drag_drop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.color_drag_enter);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.move_history)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.black_user_input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.white_user_input)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label color_label_1;
+        private System.Windows.Forms.Label guess_label_1;
+        private System.Windows.Forms.Label color_label_4;
+        private System.Windows.Forms.Label color_label_2;
+        private System.Windows.Forms.Label color_label_5;
+        private System.Windows.Forms.Label color_label_6;
+        private System.Windows.Forms.Label color_label_3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label heading_label;
+        private System.Windows.Forms.Label guess_label_2;
+        private System.Windows.Forms.Label guess_label_3;
+        private System.Windows.Forms.Label guess_label_4;
+        private System.Windows.Forms.Label guess_label_5;
+        private System.Windows.Forms.DataGridView move_history;
+        private System.Windows.Forms.Button start_game_button;
+        private System.Windows.Forms.NumericUpDown black_user_input;
+        private System.Windows.Forms.NumericUpDown white_user_input;
+        private System.Windows.Forms.Button manual_evaluation_button;
+        private System.Windows.Forms.Button automatic_evaluate_button;
+    }
+}
+
---
+++ Programy/09 - Hra logik/Mastermind/Mastermind/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Logik
+{
+    public partial class Form1 : Form {
+        // The generated solutions
+        string[] solutions = new string[(int)Math.Pow(6, 5)];
+
+        // Number of remaining possible solutions
+        int solutions_remaining = 0;
+
+        // How many of the colors did the user specify
+        int user_specified_colors = 0;
+
+        // Computer's current guess
+        string guess = "";
+
+        // Random number generator to generate guesses
+        Random random = new Random();
+
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Gets the back color from the drag-and-droppable palette.
+        /// </summary>
+        private void get_color(object sender, MouseEventArgs e) {
+            DoDragDrop((sender as Label).BackColor, DragDropEffects.All);
+        }
+
+
+        /// <summary>
+        /// Called when drag and drop enters the user input cells.
+        /// </summary>
+        private void color_drag_enter(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(typeof(Color)))
+                e.Effect = DragDropEffects.Move;
+        }
+
+
+        /// <summary>
+        /// Called when the user drops the drag-and-drop into the input cell.
+        /// </summary>
+        private void color_drag_drop(object sender, DragEventArgs e) {
+            // If there was no color previously, increment user_specified_colors
+            if ((sender as Label).BackColor == Color.Silver)
+                user_specified_colors++;
+
+            // Set the background color to drop
+            (sender as Label).BackColor = (Color)e.Data.GetData(typeof(Color));
+
+            // Enable automatic evaluation if all colors have been specified
+            if (user_specified_colors == 5  && guess != "")
+                automatic_evaluate_button.Enabled = true;
+        }
+
+
+        /// <summary>
+        /// Generates solutions, takes a guess, enables buttons.
+        /// </summary>
+        private void start_game_button_Click(object sender, EventArgs e) {
+            // Enable user inputs and disable startgame
+            manual_evaluation_button.Enabled = true;
+            black_user_input.Enabled = true;
+            white_user_input.Enabled = true;
+            start_game_button.Enabled = false;
+            if (user_specified_colors == 5)
+                automatic_evaluate_button.Enabled = true;
+
+            // Recursively generate all possible solutions
+            generate_solutions(new int[5], 4);
+
+            // Add columns to the DataGridView
+            for(int i = 0; i < 5; i++) {
+                DataGridViewImageColumn column = new DataGridViewImageColumn();
+                move_history.Columns.Add(column);
+            }
+
+            // Make the first guess
+            generate_guess();
+        }
+
+
+        /// <summary>
+        /// Creates a row of the specified color sequence.
+        /// </summary>
+        /// <param name="color_sequence">The sequence of chars from 0 to 5 
+        /// representing the colors.</param>
+        private void add_to_history(string color_sequence) {
+            move_history.Rows.Add();
+
+            int column = move_history.RowCount - 1;
+
+            for (int i = 0; i < 5; i++) {
+                int row = i;
+
+                // Create the image and add it to the last row
+                Bitmap img = new Bitmap("img/" + color_sequence[i] + ".png");
+                move_history.Rows[column].Cells[row].Value = img;
+            }
+
+            // Scroll down the DataGridView
+            move_history.FirstDisplayedScrollingRowIndex = column;
+        }
+
+
+        /// <summary>
+        /// A recursive function that generates all the possible combinations.
+        /// </summary>
+        private void generate_solutions(int[] solution, int depth) {
+            if (depth < 0) {    // If we reached the bottom, add the solution
+                solutions[solutions_remaining] = string.Join("", solution);
+                solutions_remaining++;
+            } else {            // Else recursively call all possible variations
+                for (int i = 0; i < 6; i++) {
+                    solution[depth] = i;
+                    generate_solutions(solution, depth - 1);
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Evaluates two color sequences and returns their score.
+        /// </summary>
+        /// <param name="color_sequence_1">The first color sequence.</param>
+        /// <param name="color_sequence_2">The second color sequence.</param>
+        /// <returns>int[2], first is black and second white </returns>
+        private int[] compare_inputs(String color_sequence_1, String color_sequence_2) {
+            int black = 0, white = 0;
+            int[] color_frequency = new int[6];
+
+            for(int i = 0; i < color_sequence_1.Length; i++) {
+                // If it's a direct match, increment black
+                // Else if there is an indirect match, increment white
+                if (color_sequence_1[i] == color_sequence_2[i]) black++;
+                else {
+                    // The algorithm works in a way that 
+                    color_frequency[color_sequence_1[i] - '0'] += 1;
+                    if (color_frequency[color_sequence_1[i] - '0'] <= 0)
+                        white += 1;
+
+                    color_frequency[color_sequence_2[i] - '0'] -= 1;
+                    if (color_frequency[color_sequence_2[i] - '0'] >= 0)
+                        white += 1;
+                }
+            }
+
+            return new int[] {black, white};
+        }
+
+
+        /// <summary>
+        /// Calls evaluate() with the values from the appropriate updowns.
+        /// </summary>
+        private void manual_evaluation_button_Click(object sender, EventArgs e) {
+            evaluate(new int[] {(int)black_user_input.Value,
+                                (int)white_user_input.Value});
+        }
+
+
+        /// <summary>
+        /// Calls evaluate() with automatically evaluated user input.
+        /// </summary>
+        private void automatic_evaluate_button_Click(object sender, EventArgs e) {
+            evaluate(compare_inputs(get_user_input(), guess));
+        }
+
+
+        /// <summary>
+        /// Converts user input into a string of digits representing colors.
+        /// </summary>
+        /// <returns>String of digits (0 to 5) representing the colors</returns>
+        private string get_user_input() {
+            // A dictionary to convert colors to strings
+            Dictionary<Color, string> color_to_string = new Dictionary<Color, string>() {
+                { color_label_1.BackColor, "0" },
+                { color_label_2.BackColor, "1" },
+                { color_label_3.BackColor, "2" },
+                { color_label_4.BackColor, "3" },
+                { color_label_5.BackColor, "4" },
+                { color_label_6.BackColor, "5" }
+            };
+
+            // Return the colors converted to a string
+            return color_to_string[guess_label_1.BackColor] +
+                   color_to_string[guess_label_2.BackColor] +
+                   color_to_string[guess_label_3.BackColor] +
+                   color_to_string[guess_label_4.BackColor] +
+                   color_to_string[guess_label_5.BackColor];
+        }
+
+
+        /// <summary>
+        /// Evaluates user input.
+        /// </summary>
+        /// <param name="guess_score">The score of the guess.</param>
+        private void evaluate(int[] guess_score) {
+            // See if we won 
+            if (guess_score.SequenceEqual(new int[]{5, 0})) {
+                MessageBox.Show("I won! It took me " + move_history.RowCount.ToString() + " guesses.");
+                Application.Exit();
+            }
+
+            // Remove incorrect solutions by swapping them with last remaining solution
+            for (int i = 0; i < solutions_remaining;) {
+                string solution = solutions[i];
+
+                if (!compare_inputs(solution, guess).SequenceEqual(guess_score)) {
+                    solutions[i] = solutions[solutions_remaining - 1];
+                    solutions_remaining--;
+                }
+                else i++;
+            }
+
+            // Check if there are any guesses left (if the opponent isn't cheating)
+            if (solutions_remaining == 0) {
+                MessageBox.Show("You're cheating, I'm not playing!");
+                Application.Exit();
+            }
+
+            // Makes a new guess
+            generate_guess();
+        }
+
+
+        /// <summary>
+        /// Generates a guess for the computer and adds it to the DataGridView
+        /// </summary>
+        private void generate_guess() {
+            guess = solutions[random.Next(solutions_remaining)];
+            add_to_history(guess);
+        }
+
+
+        /// <summary>
+        /// Adjust maximum value for the white_user_input updown based on the black one.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void black_user_input_ValueChanged(object sender, EventArgs e) {
+            black_user_input.Maximum = 5;
+
+            if (black_user_input.Value >= 4) white_user_input.Maximum = 0;
+            else white_user_input.Maximum = 5 - black_user_input.Value;
+        }
+    }
+}
