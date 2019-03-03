74be60323904926dad4f7febda37d040ce9f064c
Unknown <tomas.slama.131@gmail.com>
2019-03-03 22:24:43 +0100

    update permutations program

---
+++ Programy/08 - Generování další permutace/permutations.py
@@ -0,0 +1,35 @@
+def permutations(array):
+    """Generates all lexicographic permutations of a list of numbers."""
+
+    # go from the end till the numbers stop rising
+    i = len(array) - 1
+    while i >= 1:
+        if array[i - 1] < array[i]:
+            # find the next bigger number from the end to [i - 1] and swap it
+            for j in reversed(range(i, len(array))):
+                if array[j] > array[i - 1]:
+                    array[i - 1], array[j] = array[j], array[i - 1]
+
+                    # reverse the numbers from the end that are consecutive
+                    for k in range(i, len(array) - (len(array) - i) // 2):
+                        reverseK = i + (len(array) - k - 1)
+
+                        array[k], array[reverseK] = array[reverseK], array[k]
+
+                    # print the new permutation and start the process over
+                    yield array
+                    i = len(array) - 1
+                    break
+        else:
+            i -= 1
+    return None
+
+
+size = int(input("How big of a list to generate permutations of: "))
+list = [x for x in range(1, size + 1)]
+
+print(list)
+for permutation in permutations(list):
+    print(permutation)
+
+input()
