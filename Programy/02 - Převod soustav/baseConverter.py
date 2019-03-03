3788f2df05be099914e34d3b6b09768069276b29
Unknown <tomas.slama.131@gmail.com>
2019-03-03 22:42:38 +0100

    update baseConverter.py
    The code should perform much better now, thanks to the change in conversion from the input number to base 10.
    
    The code is also more readable this way.

--- Programy/02 - Převod soustav/baseConverter.py
+++ Programy/02 - Převod soustav/baseConverter.py
@@ -1,19 +1,21 @@
-chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+chars = "0123456789ABCDEF"
 
-# Get the information for conversion
-number = list(reversed(input("The number to convert: ")))
+# get conversion information
+numberString = input("The number to convert: ")
 baseFrom = int(input("Base to convert from: "))
 baseTo = int(input("Base to convert to: "))
 
-# Converts the number to base 10
-sum = sum([chars.index(number[i]) * baseFrom**i for i in range(0, len(number))])
+# converts the number to base 10
+sum = 0
+for char in numberString:
+    sum = sum * baseFrom + chars.index(char)
 
-# Convers the number to the specified base
+# convers the number to the specified base
 result = ""
 while sum > 0:
     result = chars[int(sum % baseTo)] + result
     sum = (sum - (sum % baseTo)) // baseTo
 
-print("Number "+"".join(reversed(number))+" in base "+str(baseTo)+" is "+result)
+print("Number "+numberString+" in base "+str(baseTo)+" is "+result)
 
 input()
