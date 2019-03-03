56fc00af43ae1fc5778b49bf18c1a967078a7da0
Unknown <tomas.slama.131@gmail.com>
2019-03-03 22:25:33 +0100

    update factorial.py
    Add special case for 0, adjust comment style.

--- Programy/01 - Factorial/factorial.py
+++ Programy/01 - Factorial/factorial.py
@@ -1,26 +1,31 @@
 factorial = int(input("How big of a factorial to calculate: "))
 
-# Reverse the input (and convert to numbers) to operate easily
+# reverse the input (and convert to numbers) to operate easily
 result = [int(char) for char in reversed(str(factorial))]
 
-# Multiply by all the numbers from 2 to i - 1
+# multiply by all the numbers from 2 to i - 1
 for i in range(2, factorial):
     carry = 0
 
-    # Like normal multiplication - multiply digits, carry the remainder
-    for j in range(0, len(result)):
+    # like normal multiplication - multiply digits, carry the remainder
+    for j in range(len(result)):
         result[j] = result[j] * i + carry
         carry = result[j] // 10
         result[j] %= 10
 
-    # If the carry is non-zero, create new digits
+    # if the carry is non-zero, create new digits
     while carry != 0:
         result.append(carry % 10)
         carry //= 10
 
-# Print the result (it's reversed, so print in reverse)
+# print the result (it's reversed, so print in reverse)
 print(str(factorial)+"! = ", end="")
-for number in reversed(result):
-    print(number, end="")
+
+# 0! = 1
+if factorial == 0:
+    print("1")
+else:
+    for number in reversed(result):
+        print(number, end="")
 
 input()
